a2f40499a37cf59e6e12834f98034583d82fe57e
Moahmmed Zubair Sanadi <136846638+Zubairs7@users.noreply.github.com>
2024-07-11 17:29:46 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+      window.location.href = "https://zubairs7.github.io/PeacePath/"; // Replace with your actual home page URL
+    }, 6000); // Adjust timing as needed to match CSS animation duration
+  });
+
